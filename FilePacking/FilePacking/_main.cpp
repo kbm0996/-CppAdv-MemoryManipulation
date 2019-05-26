ecdd9ae73af4d0c8880cf7c1fcab0479f419178c
KB <kbm0996@naver.com>
2019-05-27 03:07:55 +0900

    edit variable name

--- FilePacking/FilePacking/_main.cpp
+++ FilePacking/FilePacking/_main.cpp
@@ -5,14 +5,14 @@
 #include <conio.h>
 
 // Dest File(Packing File) Header
-struct st_LOCAL_FILE_HEADER
+struct st_CENTRAL_FILE_HEADER
 {
 	int iSignature;  // fixed 0x03034a50
 	int iFileCnt;
 };
 
 // Source File Header
-struct st_CENTRAL_FILE_HEADER
+struct st_LOCAL_FILE_HEADER
 {
 	WCHAR szFileName[128];
 	int iFileSize;			
@@ -49,11 +49,11 @@ void main(void)
 			WCHAR ** szNameArray = (WCHAR**)malloc(sizeof(WCHAR*) * iFileCnt);
 			for (int iCnt = 0; iCnt < iFileCnt; ++iCnt) 
 			{
-				szNameArray[iCnt] = (WCHAR*)malloc(sizeof(st_CENTRAL_FILE_HEADER::szFileName));
-				memset(szNameArray[iCnt], 0, sizeof(st_CENTRAL_FILE_HEADER::szFileName));
+				szNameArray[iCnt] = (WCHAR*)malloc(sizeof(st_LOCAL_FILE_HEADER::szFileName));
+				memset(szNameArray[iCnt], 0, sizeof(st_LOCAL_FILE_HEADER::szFileName));
 
 				wprintf(L"Input %d File Name : ", iCnt);
-				wscanf_s(L"%s", szNameArray[iCnt], (unsigned)_countof(st_CENTRAL_FILE_HEADER::szFileName));
+				wscanf_s(L"%s", szNameArray[iCnt], (unsigned)_countof(st_LOCAL_FILE_HEADER::szFileName));
 			}
 
 			WCHAR szPackingFile[128];
@@ -87,7 +87,7 @@ void main(void)
 void FilePacking(WCHAR * szPackingFile, int iElementFileCnt, WCHAR ** szElementNameArray)
 {
 	// 0. Packing File Header Setting
-	st_LOCAL_FILE_HEADER PackHeader;
+	st_CENTRAL_FILE_HEADER PackHeader;
 	PackHeader.iSignature = 0x03034a50;
 	PackHeader.iFileCnt = iElementFileCnt;
 
@@ -112,20 +112,20 @@ void FilePacking(WCHAR * szPackingFile, int iElementFileCnt, WCHAR ** szElementN
 		}
 	}
 
-	// 3. write st_LOCAL_FILE_HEADER
+	// 3. write st_CENTRAL_FILE_HEADER
 	int iMaxFile = 0;
-	st_CENTRAL_FILE_HEADER FileInfo;
-	fwrite(&PackHeader, sizeof(st_LOCAL_FILE_HEADER), 1, pDestFile);
+	st_LOCAL_FILE_HEADER LocalHeader;
+	fwrite(&PackHeader, sizeof(st_CENTRAL_FILE_HEADER), 1, pDestFile);
 	for (int iCnt = 0; iCnt < iElementFileCnt; ++iCnt)
 	{
 		fseek(pSourceFile[iCnt], 0, SEEK_END); // file size
-		FileInfo.iFileSize = ftell(pSourceFile[iCnt]);
-		wcscpy_s(FileInfo.szFileName, szElementNameArray[iCnt]);
+		LocalHeader.iFileSize = ftell(pSourceFile[iCnt]);
+		wcscpy_s(LocalHeader.szFileName, szElementNameArray[iCnt]);
 
-		fwrite(&FileInfo, sizeof(FileInfo), 1, pDestFile);
+		fwrite(&LocalHeader, sizeof(LocalHeader), 1, pDestFile);
 
-		if (iMaxFile < FileInfo.iFileSize)
-			iMaxFile = FileInfo.iFileSize;
+		if (iMaxFile < LocalHeader.iFileSize)
+			iMaxFile = LocalHeader.iFileSize;
 	}
 
 	// 4. Source File Data ------send-----> Packing File
@@ -157,8 +157,8 @@ void FileUnpacking(WCHAR * szPackingFile)
 	}
 
 	// 1. Header Check
-	st_LOCAL_FILE_HEADER PackHeader;
-	size_t iReadPos = fread(&PackHeader, sizeof(st_LOCAL_FILE_HEADER), 1, pDestFile);
+	st_CENTRAL_FILE_HEADER PackHeader;
+	size_t iReadPos = fread(&PackHeader, sizeof(st_CENTRAL_FILE_HEADER), 1, pDestFile);
 	if (iReadPos != 1)
 	{
 		wprintf(L"Packing File Local Header Error\n");
@@ -175,15 +175,15 @@ void FileUnpacking(WCHAR * szPackingFile)
 
 
 	// 2. Read Header - Get Source File Info
-	iReadPos = sizeof(st_CENTRAL_FILE_HEADER) * PackHeader.iFileCnt;
-	st_CENTRAL_FILE_HEADER * FileInfo = (st_CENTRAL_FILE_HEADER*)malloc(iReadPos);
-	memset(FileInfo, 0, iReadPos);
+	iReadPos = sizeof(st_LOCAL_FILE_HEADER) * PackHeader.iFileCnt;
+	st_LOCAL_FILE_HEADER * LocalHeader = (st_LOCAL_FILE_HEADER*)malloc(iReadPos);
+	memset(LocalHeader, 0, iReadPos);
 
-	iReadPos = fread(FileInfo, iReadPos, 1, pDestFile);
+	iReadPos = fread(LocalHeader, iReadPos, 1, pDestFile);
 	if (iReadPos != 1) {
 		wprintf(L"Packing File Central Header Source File Info Error\n");
 		fclose(pDestFile);
-		free(FileInfo);
+		free(LocalHeader);
 		return;
 	}
 
@@ -191,27 +191,27 @@ void FileUnpacking(WCHAR * szPackingFile)
 	int iMaxSize = 0;
 	for (int iCnt = 0; iCnt < PackHeader.iFileCnt; ++iCnt)
 	{
-		if (iMaxSize < FileInfo[iCnt].iFileSize)
-			iMaxSize = FileInfo[iCnt].iFileSize;
+		if (iMaxSize < LocalHeader[iCnt].iFileSize)
+			iMaxSize = LocalHeader[iCnt].iFileSize;
 	}
 	char* pBuffer = (char*)malloc(iMaxSize);
 
 	// 4. Get Source File
 	for (int iCnt = 0; iCnt < PackHeader.iFileCnt; ++iCnt)
 	{
-		wprintf(L"`%s` Size : %d \n", FileInfo[iCnt].szFileName, FileInfo[iCnt].iFileSize);
+		wprintf(L"`%s` Size : %d \n", LocalHeader[iCnt].szFileName, LocalHeader[iCnt].iFileSize);
 
 		FILE* pSourceFile;
-		if (_wfopen_s(&pSourceFile, FileInfo[iCnt].szFileName, L"wb") != 0)
+		if (_wfopen_s(&pSourceFile, LocalHeader[iCnt].szFileName, L"wb") != 0)
 		{
-			wprintf(L"Can't Open Source File `%s`\n", FileInfo[iCnt].szFileName);
+			wprintf(L"Can't Open Source File `%s`\n", LocalHeader[iCnt].szFileName);
 		}
-		fread(pBuffer, FileInfo[iCnt].iFileSize, 1, pDestFile);
-		fwrite(pBuffer, FileInfo[iCnt].iFileSize, 1, pSourceFile);
+		fread(pBuffer, LocalHeader[iCnt].iFileSize, 1, pDestFile);
+		fwrite(pBuffer, LocalHeader[iCnt].iFileSize, 1, pSourceFile);
 		fclose(pSourceFile);
 	}
 
 	fclose(pDestFile);
 	free(pBuffer);
-	free(FileInfo);
+	free(LocalHeader);
 }
